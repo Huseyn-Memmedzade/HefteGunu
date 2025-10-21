bdd8a6fd50c65ec339d887f1a9f92744f81425a2
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-10-21 17:45:36 +0400

    Add files via upload

---
+++ HefteGunu.java
@@ -0,0 +1,14 @@
+package map;
+
+public enum HefteGunu {
+
+    BIRINCI_GUN,
+    IKINCI_GUN,
+    UCUNCU_GUN,
+    DORDUNCU_GUN,
+    BESINCI_GUN,
+    ALTINCI_GUN,
+    BAZAR_GUNU;
+
+
+}
---
+++ Main8.java
@@ -0,0 +1,15 @@
+package map;
+
+public class Main8 {
+
+
+    public static void main(String[] args) {
+
+
+        HefteGunu bugun = HefteGunu.UCUNCU_GUN;
+        System.out.println(bugun);
+
+    }
+
+
+}
